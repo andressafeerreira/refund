085150871cc042c4c368b1a05746c88823c39be2
Andressa Ferreira <andressaferre03@gmail.com>
2025-01-20 13:51:44 -0300

    Criando elemento de li e adicionado item ul

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+// Seleciona os elementos da lista.
+const expenseList = document.querySelector("ul")
+
 
 //Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -39,10 +42,35 @@ form.onsubmit = (event) => {
     id: new Date().getTime(),
     expense: expense.value,
     category_id: category.value,
-    category_name: category.options[category.selectedIndex.text],
+    category_name: category.options[category.selectedIndex].text,
     amount: amount.value,
     created_at: new Date(),
   }
 
-  console.log(newExpense)
+  // Chama a função que irá adicionar o item na lista
+  expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense){
+  try {
+    // Cria o elemento de li para adicionar o item na lista(ul).
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+
+    //Cria o ícone da categoria.
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    // Adiciona as informações no item.
+    expenseItem.append(expenseIcon)
+
+    // Adiciona o item na lista.
+    expenseList.append(expenseItem)
+    
+  } catch(error) {
+    alert("Não foi possível atualizar a lista de despesas.")
+    console.log(error)
+  }
+
 }
